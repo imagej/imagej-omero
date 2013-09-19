e57a6aaa5ccea2dbc6dfb4e26ef4974318e9d9c9
Curtis Rueden <ctrueden@wisc.edu>
2013-09-19 16:57:55 -0500

    Add utility class for managing module identifiers

---
+++ src/main/java/imagej/omero/ModuleUtils.java
@@ -0,0 +1,55 @@
+/*
+ * #%L
+ * Server- and client-side communication between ImageJ and OMERO.
+ * %%
+ * Copyright (C) 2013 Board of Regents of the University of
+ * Wisconsin-Madison.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package imagej.omero;
+
+import imagej.Identifiable;
+import imagej.module.ModuleInfo;
+import imagej.module.ModuleService;
+
+/**
+ * Utility class for working with ImageJ modules, particularly
+ * {@link Identifiable} ones.
+ * 
+ * @author Curtis Rueden
+ */
+public final class ModuleUtils {
+
+	private ModuleUtils() {
+		// NB: Prevent instantiation of utility class.
+	}
+
+	/** Looks up an {@link Identifiable} ImageJ module by its identifier. */
+	public static ModuleInfo findModule(final ModuleService moduleService,
+		final String id)
+	{
+		// TODO: Migrate logic to ModuleService, and cache identifiers in a hash.
+		for (final ModuleInfo info : moduleService.getModules()) {
+			if (!(info instanceof Identifiable)) continue;
+			final String infoID = ((Identifiable) info).getIdentifier();
+			if (id.equals(infoID)) return info;
+		}
+		throw new IllegalArgumentException("No module for ID: " + id);
+	}
+
+}
