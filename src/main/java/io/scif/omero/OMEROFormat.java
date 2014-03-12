1b00d4bbefb3e70c9050212cd25598e3f77e798f
Curtis Rueden <ctrueden@wisc.edu>
2014-03-12 16:32:05 +0100

    OMEROFormat: fix width & height determination bug

--- src/main/java/io/scif/omero/OMEROFormat.java
+++ src/main/java/io/scif/omero/OMEROFormat.java
@@ -432,8 +432,8 @@ public class OMEROFormat extends AbstractFormat {
 				// FIXME: Check before array access, and before casting.
 				final int x = (int) planeMin[0];
 				final int y = (int) planeMin[1];
-				final int w = (int) (planeMax[0] - planeMin[0] + 1);
-				final int h = (int) (planeMax[1] - planeMin[1] + 1);
+				final int w = (int) (planeMax[0] - planeMin[0]);
+				final int h = (int) (planeMax[1] - planeMin[1]);
 				final byte[] tile = store.getTile(z, c, t, x, y, w, h);
 				plane.setData(tile);
 			}
