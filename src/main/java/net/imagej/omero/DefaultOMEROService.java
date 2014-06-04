90eec1ad984a6bad992995fc7f4e62749f2ab90c
Curtis Rueden <ctrueden@wisc.edu>
2014-06-04 16:24:30 +0200

    DefaultOMEROService: add 'omero:' prefix
    This is for consistency with the reader.

--- src/main/java/net/imagej/omero/DefaultOMEROService.java
+++ src/main/java/net/imagej/omero/DefaultOMEROService.java
@@ -346,7 +346,7 @@ public class DefaultOMEROService extends AbstractService implements
 		// Will need to rethink how SCIFIO conveys source and destination metadata.
 		// The RandomAccessInput/OutputStream design is probably too narrow.
 		final String omeroDestination =
-			credentials(client) //
+			"omero:" + credentials(client) //
 				+ ".omero"; // FIXME: Remove this after SCIFIO doesn't need it anymore.
 
 		final Metadata metadata = datasetIOService.save(dataset, omeroDestination);
