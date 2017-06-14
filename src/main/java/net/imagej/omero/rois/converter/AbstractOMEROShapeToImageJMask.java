6a25701432626f28c7b899c470a1245685808624
Alison Walter <awalter2@wisc.edu>
2017-06-14 09:19:15 -0500

    Add base class for converting ShapeData to Mask

---
+++ src/main/java/net/imagej/omero/rois/converter/AbstractOMEROShapeToImageJMask.java
@@ -0,0 +1,66 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2013 - 2016 Open Microscopy Environment:
+ * 	- Board of Regents of the University of Wisconsin-Madison
+ * 	- Glencoe Software, Inc.
+ * 	- University of Dundee
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package net.imagej.omero.rois.converter;
+
+import net.imglib2.roi.RealMask;
+
+import org.scijava.convert.AbstractConverter;
+
+import omero.gateway.model.ShapeData;
+
+/**
+ * Abstract base class for converting OMERO {@link ShapeData} to
+ * {@link RealMask}.
+ *
+ * @author Alison Walter
+ * @param <S> OMERO Shape input type
+ * @param <M> {@link RealMask} output type
+ */
+public abstract class AbstractOMEROShapeToImageJMask<S extends ShapeData, M extends RealMask>
+	extends AbstractConverter<S, M>
+{
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <T> T convert(final Object src, final Class<T> dest) {
+		if (src == null || dest == null) throw new NullPointerException();
+		if (!getInputType().isInstance(src)) {
+			throw new IllegalArgumentException("Expected: " + getInputType()
+				.getSimpleName() + " Received: " + src.getClass().getSimpleName());
+		}
+		if (!dest.isAssignableFrom(getOutputType())) {
+			throw new IllegalArgumentException("Expected: " + getOutputType()
+				.getSimpleName() + " Received: " + dest.getSimpleName());
+		}
+		final S shape = (S) src;
+		final M result = convert(shape);
+
+		return (T) result;
+	}
+
+	abstract M convert(S shape);
+
+}
