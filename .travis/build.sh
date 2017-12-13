7df43265c8fce016b4b2ed508464cf5543053905
jmoore <josh@glencoesoftware.com>
2017-12-13 18:48:14 +0100

    Make sure .m2 exists

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Required for mounting m2 into docker
+mkdir -p $HOME/.m2
 chmod a+rw $HOME/.m2
 
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
