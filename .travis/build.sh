e50cdaa0ec310253f81883ab02b23ac74712b84d
jmoore <josh@glencoesoftware.com>
2017-12-13 12:46:33 +0100

    Introduce OMERO integration test framework
     * Extends build.sh to clone omero-test-infra and
       invoke lib-docker, a framework for testing
       libraries which require a running OMERO instance
       for integration tests
     * Adds a Dockerfile which at the moment only
       builds imagej-omero but a command to run
       integration can be added later to the end
       of the Dockerfile to connect to host "omero"

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
 sh travis-build.sh
+
+git clone git://github.com/openmicroscopy/omero-test-infra .omero
+.omero/lib-docker
