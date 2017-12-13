a5a48be5abddb0325857575bd402671076275edf
jmoore <josh@glencoesoftware.com>
2017-12-13 18:19:47 +0100

    Chmod .m2 for mounting to docker

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# Required for mounting m2 into docker
+chmod a+rw $HOME/.m2
+
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
 sh travis-build.sh
 
