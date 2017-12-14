9f2139cd0c5e54571a43f4c1920dbeb3f767bc92
Alison Walter <awalter2@wisc.edu>
2017-12-14 12:22:58 +0100

    Add test for connecting to OMERO

---
+++ src/test/java/net/imagej/omero/it/OmeroIT.java
@@ -0,0 +1,23 @@
+
+package net.imagej.omero.it;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import Glacier2.CannotCreateSessionException;
+import Glacier2.PermissionDeniedException;
+import omero.ServerError;
+
+@Category(IntegrationTest.class)
+public class OmeroIT {
+
+	private omero.client client;
+
+	@Test
+	public void testConnectingToServer() throws CannotCreateSessionException,
+		PermissionDeniedException, ServerError
+	{
+		client = new omero.client("localhost", 4064);
+		client.createSession("root", "omero");
+	}
+}
