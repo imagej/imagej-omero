f1ed4041ae3874ae43005650e2ab36953b51ab66
jmoore <josh@glencoesoftware.com>
2018-02-13 14:39:04 +0000

    Backup/restore support
    This splits the omero-test-infra logic out into a new
    .travis/test.sh script which can be called indepdently.
    It's designed to allow travis to download an archive
    and restore the state of OMERO.
    
    See https://github.com/openmicroscopy/omero-test-infra/pull/8

---
+++ .travis/test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Note: script has been extracted from build.sh to simplify local testing
+
+set -e
+set -x
+
+export BACKUPURL=${BACKUPURL:-http://test.imagej.net/omero/omero_test_infra_backup.zip}
+export INFRABRANCH=${INFRABRANCH:-v0.3.1}
+export INFRAREPO=${INFRAREPO:-git://github.com/openmicroscopy/omero-test-infra}
+
+if [ ! -d .omero ];
+then
+    git clone -b ${INFRABRANCH} ${INFRAREPO} .omero
+fi
+
+# FIXME: setting DOCKER_ARGS here should work but it's not.
+# It fails with:
+# [ERROR] Could not create local repository at /home/mvn/.m2/repository -> [Help 1]
+# env DOCKER_ARGS="-v $HOME/.m2:/home/mvn/.m2"
+
+export OMERO_SERVER_VERSION=5.4
+export OMERO_WEB_VERSION=5.4
+export COMPOSE_FILE="docker-compose.yml:volumes.yml"
+
+.omero/docker lib
+
+# Optionally persist for a next run
