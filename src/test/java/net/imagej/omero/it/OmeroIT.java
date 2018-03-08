b76a7eb440881d30deb9443310307e1826c686d1
Alison Walter <awalter2@wisc.edu>
2018-03-08 11:02:58 -0600

    Add integration tests for ROIs

--- src/test/java/net/imagej/omero/it/OmeroIT.java
+++ src/test/java/net/imagej/omero/it/OmeroIT.java
@@ -11,12 +11,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -26,13 +26,17 @@
 package net.imagej.omero.it;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import com.google.common.collect.Lists;
 
 import io.scif.services.DatasetIOService;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import net.imagej.Dataset;
@@ -40,9 +44,18 @@ import net.imagej.omero.DefaultOMEROSession;
 import net.imagej.omero.OMEROLocation;
 import net.imagej.omero.OMEROService;
 import net.imagej.omero.OMEROSession;
+import net.imagej.omero.rois.DataNode;
+import net.imagej.omero.rois.DefaultDataNode;
+import net.imagej.omero.rois.OMERORoiCollection;
 import net.imagej.table.ByteTable;
 import net.imagej.table.DefaultByteTable;
 import net.imagej.table.Table;
+import net.imglib2.roi.geom.GeomMasks;
+import net.imglib2.roi.geom.real.Box;
+import net.imglib2.roi.geom.real.Ellipsoid;
+import net.imglib2.roi.geom.real.PointMask;
+import net.imglib2.roi.geom.real.Polygon2D;
+import net.imglib2.roi.geom.real.WritablePointMask;
 
 import org.junit.After;
 import org.junit.Before;
@@ -52,6 +65,7 @@ import org.scijava.Context;
 
 import Glacier2.CannotCreateSessionException;
 import Glacier2.PermissionDeniedException;
+import edu.emory.mathcs.backport.java.util.Collections;
 import omero.ServerError;
 import omero.gateway.exception.DSAccessException;
 import omero.gateway.exception.DSOutOfServiceException;
@@ -161,11 +175,8 @@ public class OmeroIT {
 		CannotCreateSessionException, ExecutionException, DSOutOfServiceException,
 		DSAccessException
 	{
-		final byte[][] d = new byte[][] {
-			{ 127, 0, -128 },
-			{ -1, -6, -23 },
-			{ 100, 87, 4 }
-		};
+		final byte[][] d = new byte[][] { { 127, 0, -128 }, { -1, -6, -23 }, { 100,
+			87, 4 } };
 		final ByteTable table = new DefaultByteTable(3, 3);
 		for (int c = 0; c < table.getColumnCount(); c++) {
 			table.setColumnHeader(c, "Heading " + (c + 1));
@@ -200,5 +211,82 @@ public class OmeroIT {
 		final long newId = omero.uploadTable(cred, "table-version2", ijTable, 1);
 
 		assertTrue(originalId != newId);
+	}
+
+	@Test
+	public void testDownloadROIs() throws ServerError, PermissionDeniedException,
+		CannotCreateSessionException, ExecutionException, DSOutOfServiceException,
+		DSAccessException
+	{
+		final List<DataNode<?>> dns = omero.downloadROIs(cred, 1);
+
+		assertNotNull(dns);
+		assertFalse(dns.isEmpty());
+		assertEquals(10, dns.size());
+
+		for (final DataNode<?> dn : dns) {
+			assertTrue(dn instanceof OMERORoiCollection);
+			final List<DataNode<?>> children = dn.children();
+			assertEquals(1, children.size());
+			assertTrue(children.get(0).getData() instanceof PointMask);
+		}
+	}
+
+	@Test
+	public void testDownloadROI() throws DSOutOfServiceException,
+		DSAccessException, ExecutionException
+	{
+		final DataNode<?> dn = omero.downloadROI(cred, 1);
+
+		assertTrue(dn instanceof OMERORoiCollection);
+		final List<DataNode<?>> children = dn.children();
+		assertEquals(1, children.size());
+		assertTrue(children.get(0).getData() instanceof PointMask);
+	}
+
+	@Test
+	public void testUploadROIs() throws ServerError, PermissionDeniedException,
+		CannotCreateSessionException, ExecutionException, DSOutOfServiceException,
+		DSAccessException
+	{
+		final Box b = GeomMasks.closedWritableBox(new double[] { 10, 10 },
+			new double[] { 22, 46.5 });
+		final DataNode<Box> dnb = new DefaultDataNode<>(b, null, null);
+		final Ellipsoid e = GeomMasks.openWritableEllipsoid(new double[] { 120,
+			121.25 }, new double[] { 4, 9 });
+		final DataNode<Ellipsoid> dne = new DefaultDataNode<>(e, null, null);
+		final Polygon2D p = GeomMasks.writablePolygon2D(new double[] { 30, 40, 50 },
+			new double[] { 50, 80, 50 });
+		final DataNode<Polygon2D> dnp = new DefaultDataNode<>(p, null, null);
+
+		final List<DataNode<?>> dns = Lists.newArrayList(dnb, dne, dnp);
+
+		final long[] ids = omero.uploadROIs(cred, dns, 2);
+
+		assertEquals(3, ids.length);
+		assertTrue(ids[0] > 0);
+		assertTrue(ids[1] > 0);
+		assertTrue(ids[2] > 0);
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void testDownloadThenUploadROI() throws DSOutOfServiceException,
+		DSAccessException, ExecutionException, ServerError,
+		PermissionDeniedException, CannotCreateSessionException
+	{
+		final long originalRoiId = 3;
+		final DataNode<?> dn = omero.downloadROI(cred, originalRoiId);
+
+		final List<DataNode<?>> children = dn.children();
+		assertTrue(children.get(0).getData() instanceof WritablePointMask);
+		final WritablePointMask pm = (WritablePointMask) children.get(0).getData();
+
+		pm.setPosition(new double[] { 0, 0 });
+
+		final long[] ids = omero.uploadROIs(cred, Collections.singletonList(dn), 2);
+
+		assertEquals(1, ids.length);
+		assertTrue(originalRoiId != ids[0]);
 	}
 }
