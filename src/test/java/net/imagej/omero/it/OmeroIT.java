1a2221b4ca6a40976587843234d939207e60c21f
Curtis Rueden <ctrueden@wisc.edu>
2020-06-12 18:52:59 -0500

    Remove SSLUtils hack
    It's no longer necessary with OMERO 5.5+.

--- src/test/java/net/imagej/omero/it/OmeroIT.java
+++ src/test/java/net/imagej/omero/it/OmeroIT.java
@@ -83,10 +83,6 @@ import omero.gateway.model.TableData;
 @Category(IntegrationTest.class)
 public class OmeroIT {
 
-	static {
-		net.imagej.omero.SSLUtils.fixDisabledAlgorithms();
-	}
-
 	private omero.client client;
 	private OMEROLocation cred;
 	private Context context;
